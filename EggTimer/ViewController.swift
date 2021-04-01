7d3b0cc99851621e51e77b086b731df060c3a209
reydaniel <reynaldidaniel@outlook.com>
2021-04-01 15:01:42 +0700

    create timer countdown. Using switch case to match which button get tap and start the countdown.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTime = [
+        "softEgg": 5,
+        "mediumEgg": 8,
+        "hardEgg": 12
+    ]
+    
+    var seconds = 0
+    var timer: Timer?
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let eggName = sender.currentTitle!
+        
+        switch eggName {
+            case "Soft":
+                seconds = eggTime["softEgg"]!
+                print("Boiling Soft Egg...")
+                countdown()
+            case "Medium":
+                seconds = eggTime["mediumEgg"]!
+                print("Boiling Medium Egg...")
+                countdown()
+            case "Hard":
+                seconds = eggTime["hardEgg"]!
+                print("Boiling Hard Egg...")
+                countdown()
+            default:
+                print("Egg was gone")
+            }
+        
+    }
+    
+    func countdown() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(showMessage),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func showMessage(){
+        if seconds > 0 {
+            print("\(seconds)s remaining")
+            seconds -= 1
+        } else {
+            print("Egg Ready!")
+            timer?.invalidate()
+        }
+    }
 
 }
